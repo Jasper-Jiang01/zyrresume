80534fa5874faa909ce598f259b54911acbbd88a
jiangwenzhe02 <jiangwenzhe02@meituan.com>
2026-07-28 23:59:44 +0800

    fix: serve HomePage at root / instead of redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,36 @@
-import { redirect } from 'next/navigation'
+'use client'
 
-export default function RootPage() {
-  redirect('/home')
+import { useEffect, useState } from 'react'
+import { useCursorAura } from './home/_components/useCursorAura'
+import IntroCurtain from './home/_components/IntroCurtain'
+import Nav from './home/_components/Nav'
+import Hero from './home/_components/Hero'
+import About from './home/_components/About'
+import Work from './home/_components/Work'
+import Capabilities from './home/_components/Capabilities'
+import Contact from './home/_components/Contact'
+
+export default function HomePage() {
+  const [intro, setIntro] = useState(true)
+  useCursorAura()
+
+  useEffect(() => {
+    const timer = setTimeout(() => setIntro(false), 1450)
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      {intro && <IntroCurtain />}
+      <div className="cursorAura" />
+      <Nav />
+      <main>
+        <Hero />
+        <About />
+        <Work />
+        <Capabilities />
+        <Contact />
+      </main>
+    </>
+  )
 }
